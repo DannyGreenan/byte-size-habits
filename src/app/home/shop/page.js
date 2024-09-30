647c0048f4fb0c5bc4cd3d2097b16169fa743c99
Danny <danny.greenan@live.co.uk>
2024-09-30 16:49:43 +0100

    Added User GET to profile page

--- src/app/home/shop/page.js
+++ src/app/home/shop/page.js
@@ -1,4 +1,7 @@
+import { supabaseBrowserClient } from "@/lib/supabaseBrowserClient";
 const Shop = () => {
+  //   let { data: users } = await supabaseBrowserClient.from("users").select();
+
   return <h2>Shop Page</h2>;
 };
 
