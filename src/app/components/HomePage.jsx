a99fa9e3d93cb74900594bdf1f95fde1b4790a02
Jen <jen@example.com>
2024-10-04 14:16:00 +0100

    update the patch request

--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -7,17 +7,18 @@ import { patchUser } from "../models/profile.model";
 const HomePage = () => {
     const {loggedInUser} = useContext(UserContext);
     const [hasCoded, setHasCoded] = useState(false);
-    const [hideChat, setHideChat] = useState(true);
+    const [isVisible, setIsVisible] = useState(false);
 
 const handleClick = (event) => {
     // console.log(event.target.value)
     if (event.target.value === 'yes') {
         setHasCoded(true);
-        patchUser(loggedInUser.user_id, {total_progress: 0, todays_progress: 0, currency: loggedInUser.currency + 20, streak: loggedInUser.streak + 1, weekly_progress: 0})
+        patchUser(loggedInUser.user_id, {progress: loggedInUser.progress + loggedInUser.difficulty_time, currency: loggedInUser.currency + 20, streak: loggedInUser.streak + 1})
+        // fetchUser(loggedInUser.username)
     } else {
         setHasCoded(false);
-        setHideChat(false);
     }
+
 }
 // console.log(loggedInUser)
 
@@ -29,24 +30,40 @@ const handleClick = (event) => {
             <img alt="Tailwind CSS chat bubble component" src="/happy.png" />
           </div>
         </div>
-        <div className="chat-bubble chat-bubble-info">Did you code today?
+
+        <div className="chat-bubble chat-bubble-info">
+            Did you code today?
         <button value='yes' onClick={handleClick} className="btn btn-primary mx-2">Yes</button>
         <button value='no' onClick={handleClick} className="btn btn-primary mx-2">No</button>
         </div>
       </div>
-      <div className='chat chat-start invisible'>
+      <div className='chat chat-start'>
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img alt="Tailwind CSS chat bubble component" src="/happy.png" />
+          </div>
+        </div>
+
+        <div className="chat-bubble chat-bubble-info">
+          I found some resources to help you study...
+        </div>
+      <div className='chat chat-star'>
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
             <img alt="Tailwind CSS chat bubble component" src="/happy.png" />
           </div>
         </div>
         <div className="chat-bubble chat-bubble-info">
-          Would you like to study now?<button value='yes' onClick={handleClick} className="btn btn-primary mx-2">Yes</button>
+          Would you like to study now and start a timer?
+          <button value='yes' onClick={handleClick} className="btn btn-primary mx-2">Yes</button>
         <button value='maybe-later' onClick={handleClick} className="btn btn-primary mx-2">Maybe later</button>
         </div>
         <div className="chat-bubble chat-bubble-info">
           Would you like to start the timer?<button value='yes' onClick={handleClick} className="btn btn-primary mx-2">Yes</button>
         </div>
+      </div>
+
+      
       </div>
     </>
   );
