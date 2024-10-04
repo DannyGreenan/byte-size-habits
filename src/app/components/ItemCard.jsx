cd2791887f0eb9c933ee9ddcba80b05067afc064
Danny <danny.greenan@live.co.uk>
2024-10-04 13:38:34 +0100

    Profile userContext, itemShop style

--- src/app/components/ItemCard.jsx
+++ src/app/components/ItemCard.jsx
@@ -12,33 +12,37 @@ const ItemCard = ({ item }) => {
   };
 
   return (
-    <div
-      key={item.id}
-      className="card bg-byteLightBlue text-gray-800 shadow-xl">
-      <figure className="py-4 rounded-xl">
+    <div className="mockup-browser bg-byteOrange border font-nunito font-extrabold text-byteDark">
+      <div className="mockup-browser-toolbar">
+        <div className="input text-white">{`/${item.description}`}</div>
+      </div>
+      <figure className="flex flex-col lg:flex-row items-center bg-white py-4 px-6">
         <Image
           src={item.image}
           alt={item.description}
-          width={250}
-          height={250}
-          className="rounded-xl"
+          width={200}
+          height={200}
+          className="rounded-3xl w-40 h-40 lg:w-64 lg:h-64"
         />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{item.description}</h2>
-        <p>{item.cost}</p>
-        <div className="card-actions justify-end">
-          <button
-            onClick={handleBuy}
-            value={item.description}
-            className="btn m-4 bg-byteDarkBlue text-white hover:bg-byteLightBlue hover:text-gray-900 hover:scale-105 transition-transform duration-300 flex items-center justify-center"
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}>
-            {hover ? <FaCoins className="mr-2" /> : null}
-            Buy
-          </button>
+
+        <div className="card-body flex flex-col justify-center text-center lg:text-left mt-4 lg:mt-0 lg:ml-8">
+          <p className="text-lg mb-4">
+            <FaCoins className="mr-2" />
+            {item.cost}
+          </p>
+          <div className="card-actions">
+            <button
+              onClick={handleBuy}
+              value={item.description}
+              className="btn bg-byteDarkBlue text-white hover:bg-byteLightBlue hover:text-gray-900 hover:scale-105 transition-transform duration-300 flex items-center justify-center"
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}>
+              {hover ? <FaCoins className="mr-2" /> : null}
+              Buy
+            </button>
+          </div>
         </div>
-      </div>
+      </figure>
     </div>
   );
 };
