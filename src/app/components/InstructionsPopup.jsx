91bc8f805f0072411e25c327436df93dceae2c57
Danny <danny.greenan@live.co.uk>
2024-10-05 19:02:11 +0100

    Styling

--- src/app/components/InstructionsPopup.jsx
+++ src/app/components/InstructionsPopup.jsx
@@ -1,40 +1,39 @@
-import { useEffect, useState } from 'react';
-import Modal from 'react-modal';
+import { useEffect, useState } from "react";
+import Modal from "react-modal";
 
-const InstructionsPopup = ({modalInstruction}) => {
-	const [isOpen, setIsOpen] = useState(false);
+const InstructionsPopup = ({ modalInstruction, title }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-	const suitableInstruction = modalInstruction;
+  const suitableInstruction = modalInstruction;
 
-	useEffect(() => {
-		if (typeof window !== 'undefined') {
-			Modal.setAppElement('body'); // We need this for acessibility compliance by preventing the rest of the app from being accessible to screen readers while the modal is open
-		}
-	}, []);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      Modal.setAppElement("body"); // We need this for acessibility compliance by preventing the rest of the app from being accessible to screen readers while the modal is open
+    }
+  }, []);
 
-	const openModal = () => {
-		setIsOpen(true);
-		console.log('Modal state:', isOpen);
-	};
+  const openModal = () => {
+    setIsOpen(true);
+    console.log("Modal state:", isOpen);
+  };
 
-	const closeModal = () => setIsOpen(false);
+  const closeModal = () => setIsOpen(false);
 
-	return (
-		<div>
-			<button onClick={openModal}>Show Instruction</button>
-			<Modal
-				isOpen={isOpen}
-				onRequestClose={closeModal}
-				contentLabel='Instruction Popup'
-				className='bg-white p-5 max-w-lg mx-auto rounded-lg z-50'
-				overlayClassName='fixed inset-0 bg-black bg-opacity-75 z-50'
-			>
-				<h2>Instructions</h2>
-				<p>{suitableInstruction}</p>
-				<button onClick={closeModal}>Press Outside this Box to Close.</button>
-			</Modal>
-		</div>
-	);
+  return (
+    <div>
+      <button onClick={openModal}>{title}</button>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        contentLabel="Instruction Popup"
+        className="bg-white p-5 max-w-lg mx-auto rounded-lg z-50"
+        overlayClassName="fixed inset-0 bg-black bg-opacity-75 z-50">
+        <h2>{title}</h2>
+        <p>{suitableInstruction}</p>
+        <button onClick={closeModal}>Press Outside this Box to Close.</button>
+      </Modal>
+    </div>
+  );
 };
 
 export default InstructionsPopup;
