2444d66383de88e110aae0beff6f29966cca6a6f
Jen Warman <jen.warman85@gmail.com>
2024-10-07 19:12:11 +0100

    add instructions to pop-up

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -87,7 +87,14 @@ const Home = () => {
                 <li>
                   <a>
                     <InstructionsPopup
-                      modalInstruction="Here is the home page instruction, change the instructions text according to your need!"
+                      modalInstruction="Byte Size Habits is here to help you learn to code!
+
+Whether you're just starting out or have previous coding experience Byte Size Habits allows you to set personal learning goals and improve your programming skills. 
+
+To set your goal, simply select your preferred programming language and your virtual pet will offer you helpful resources on your chosen topic.
+
+You can edit your goal anytime on your profile page. Mastered Javascript? Why not give Typescript a try?
+"
                       title="Coding Goal"
                     />
                   </a>
@@ -95,8 +102,38 @@ const Home = () => {
                 <li>
                   <a>
                     <InstructionsPopup
-                      modalInstruction="Here is the home page instruction, change the instructions text according to your need!"
+                      modalInstruction="The difficulty setting corresponds to the time you allocate per day to your learning goal. 
+
+Select Easy (30 minutes per day), Medium (45 minutes per day) or Hard (60 minutes per day).
+
+When you’re ready to study simply go to the home page and your virtual pet will set a timer for you. You can also study in your own time but be sure to let your pet know you’ve completed your daily goal.  
+"
+                      title="Difficulty"
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <InstructionsPopup
+                      modalInstruction="Your pet wants to see you succeed at your learning goal and depends on you to keep it fully charged and happy! 
+
+Your pet’s energy will decrease over time and the only way to restore its energy in full is to complete your daily goal. 
+
+Don’t let your pet’s energy drop below 1%! You may not like what you see…"
                       title="Energy"
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <InstructionsPopup
+                      modalInstruction="Everyday you complete your learning goal you are awarded coins. 
+
+When you have accumulated enough coins you can treat your pet to a gift from the shop. 
+
+Hint: Your pet LOVES to help you study, if you opt to use the timer feature you will be awarded bonus coins.
+"
+                      title="Coins"
                     />
                   </a>
                 </li>
